67478bcf3951f5630653ff5a8351922fe5f18cee
Madeline Dutro <dutro.madeline@gmail.com>
2020-05-16 10:42:18 -0400

    adds skeleton for other api call function and for function to parse text inputs for the apis

--- isTheBookBetter/scripts/app.js
+++ isTheBookBetter/scripts/app.js
@@ -1,23 +1,29 @@
 /* eslint-disable */
 //RESOURCES:
-//
+//https://www.goodreads.com/topic/show/17893514-cors-access-control-allow-origin
 
-const searchGR = () => {
-  jQuery.ajaxPrefilter(function(options) {
-    if (options.crossDomain && jQuery.support.cors) {
-        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-    }
-  });
 
-  $.ajax({
-  url: "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index.xml?key=RnKpwA2VkQwJ2eVXmElg?q=godfather" ,
-  type: 'GET',
-  '$limit': 1
-}).done(function(data) {
-  console.log(data);
-});
+
+const encodeForURL = (string) => {
+  //Need to replace spaces with percent signs so I can pass to apis
+  //let formattedString = string.
 }
 
+const searchGR = () => {
+  let apiKey = '?key=RnKpwA2VkQwJ2eVXmElg'
+  let queryParam = '&q=godfather'
+  $.ajax({
+  url: 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index.xml' + apiKey + queryParam,
+  type: 'GET',
+  '$limit': 1
+  }).done(function(data) {
+    console.log(data);
+  });
+}
+
+const searchOMDB = () => {
+  let apiKey = '3796b8a3'
+}
 
 
 $(() => {
