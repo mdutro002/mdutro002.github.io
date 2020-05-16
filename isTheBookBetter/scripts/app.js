82d80c7745cf463a07cfdb077a1732a70ee42218
Madeline Dutro <dutro.madeline@gmail.com>
2020-05-15 23:45:18 -0400

    finally got the Goodreads api returning! format is unfamiliar, but one thing at time

--- isTheBookBetter/scripts/app.js
+++ isTheBookBetter/scripts/app.js
@@ -3,8 +3,14 @@
 //
 
 const searchGR = () => {
+  jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+  });
+
   $.ajax({
-  url: "https://www.goodreads.com/search/index.xml?q=godfather" ,
+  url: "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search/index.xml?key=RnKpwA2VkQwJ2eVXmElg?q=godfather" ,
   type: 'GET',
   '$limit': 1
 }).done(function(data) {
