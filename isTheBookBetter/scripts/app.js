9d0b55ffaf426934bd870e2b4fe95d5e94b5a336
Madeline Dutro <dutro.madeline@gmail.com>
2020-05-18 02:05:13 -0400

    adjusts movie and book images to have better ratios, outputs movie details

--- isTheBookBetter/scripts/app.js
+++ isTheBookBetter/scripts/app.js
@@ -12,6 +12,10 @@ const scrubURL = (string) => {
   return formattedString;
 }
 
+const makeModal = () => {
+  //generic makeModal function that passes in templated string to display:
+}
+
 //return specific omdb result - this will make another call to the omdb api to return top result
 const returnMovie = (imdbID) => {
   let startingURL = 'http://www.omdbapi.com/?apikey=3796b8a3'
@@ -20,32 +24,41 @@ const returnMovie = (imdbID) => {
       url: startingURL + queryParam + imdbID,
       type: 'GET',
       '$limit': 10
-    }).done(function(mov){
-      console.log(mov)
+    }).done(function(movieData){
+      outputMovie(movieData);
   });
 }
 
-const outputMovie = () => {
-  //toggle hidden class on movieRes div
+const outputMovie = (movieData) => {
+  $('#movieImg').attr('src', movieData.Poster);
+  $('#movieTitle').text(movieData.Title)
+  $year = $('<h4>').text(`Year: ${movieData.Year}`)
+  $metaScore = $('<h4>').text(`Metascore: ${movieData.Metascore}`);
+  $('#movDescrip').append($year);
+  $('#movDescrip').append($metaScore);
   $('#movieRes').toggleClass('hidden');
 }
 
 //traverses xml data returned from goodreads api and pushes to bookResult div
 const outputBookData = (xmlData) => {
-  allWorks = $(xmlData).find("work");
+  allWorks = $(xmlData).find("work"); //selects XML elements at a slightly higher level
   firstWork = allWorks[0];
-  console.log(firstWork);
-  allBooks = $(xmlData).find("best_book");
+  allBooks = $(xmlData).find("best_book"); //selects all best_book XML items
   firstBook = allBooks[0];
-  var idNode = $(firstBook).find('id').text();
+  console.log();
+  console.log();
+  var fidnode = $(firstWork).find('id').text();//TODO - which of these gets bookid?
+  var bidNode = $(firstBook).find('id').text();
   var titleNode = $(firstBook).find('title').text();
   var authorNode = $(firstBook).find('name').text();
   var imageNode = $(firstBook).find('image_url').text();
+
   $('#bookTitle').text(titleNode);
   $('#bookImg').attr('src', imageNode);
   $('#bookRes').toggleClass('hidden');
-}
 
+  //https://www.goodreads.com/book/show/
+}
 
 //goodreads search method call
 const searchGR = (searchString) => {
@@ -72,7 +85,6 @@ const searchOMDB = (searchString) => {
     type: 'GET',
     '$limit': 10
   }).done(function(data){
-    //pass top 1 result to returnMovie();
     returnMovie(data.Search[0].imdbID);
   })
 }
@@ -88,7 +100,7 @@ $(() => {
 
   $('#searchPrompt').on('click', () => {
     let searchString = $('#search').val();
-    //searchOMDB(searchString);
+    searchOMDB(searchString);
     searchGR(searchString);
 
   })
