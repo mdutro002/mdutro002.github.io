1c5a13030d4e0a93c1d321f09e778c0cde8c7a2a
Madeline Dutro <dutro.madeline@gmail.com>
2020-07-27 16:40:05 -0400

    this is less of a commit and more of a holistic overhaul - still not done, but wanted to add everything I've done so far

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+$(() => {
+  //Name In
+  $('.name').slideDown('slow').removeClass('hidden');
 
+  //AboutMe Toggle
+  $('#aboutMeToggle').click(function (){
+    $('.aboutMe').slideToggle().toggleClass('hidden');
+  });
+
+  //Projects Toggle
+  $('#projectToggle').click(function (){
+    $('.proj').slideToggle().toggleClass('hidden');
+  });
+}); //end onready
